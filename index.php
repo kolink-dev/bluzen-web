1b72984da0c138bf130f233785d765a55002060a
Afif-m91 <afif.muhamad91@gmail.com>
2023-02-27 10:48:58 +0700

    header

--- index.php
+++ index.php
@@ -18,18 +18,29 @@
     <title>Bluzen | We support your growth through digital transformation</title>
     <link rel="canonical" href="http://localhost/bluzen/" />
     <!-- MOBILE SPECIFIC -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">                                
-    <link rel="stylesheet" href="css/bootstrap.min.css"><!-- BOOTSTRAP STYLE SHEET -->    
-    <link rel="stylesheet" href="css/fontawesome/css/font-awesome.min.css"><!-- FONTAWESOME STYLE SHEET -->
-    <link rel="stylesheet" href="css/owl.carousel.min.css"><!-- OWL CAROUSEL STYLE SHEET -->
-    <link rel="stylesheet" href="css/magnific-popup.min.css"><!-- MAGNIFIC POPUP STYLE SHEET -->	
-    <link rel="stylesheet" href="css/loader.min.css"><!-- LOADER STYLE SHEET -->
-    <link rel="stylesheet" href="css/flaticon.min.css"><!-- FLATICON STYLE SHEET -->
-	<link rel="stylesheet" href="css/animate.css" ><!-- WOW ANIMATE STYLE SHEET -->
-    <link rel="stylesheet" href="css/feather.css" ><!-- Feather STYLE SHEET -->
-    <link rel="stylesheet" href="css/lc_lightbox.css"><!-- IMAGE POPUP -->  
-    <link rel="stylesheet" href="css/swiper-bundle.min.css"><!-- SWIPER SLIDER CSS -->
-    <link rel="stylesheet" href="css/style.css"><!-- MAIN STYLE SHEET -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">     
+    <!-- BOOTSTRAP STYLE SHEET -->                           
+    <link rel="stylesheet" href="css/bootstrap.min.css">    
+    <!-- FONTAWESOME STYLE SHEET -->
+    <link rel="stylesheet" href="css/fontawesome/css/font-awesome.min.css">
+    <!-- OWL CAROUSEL STYLE SHEET -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <!-- MAGNIFIC POPUP STYLE SHEET -->	
+    <link rel="stylesheet" href="css/magnific-popup.min.css">
+    <!-- LOADER STYLE SHEET -->
+    <link rel="stylesheet" href="css/loader.min.css">
+    <!-- FLATICON STYLE SHEET -->
+    <link rel="stylesheet" href="css/flaticon.min.css">
+    <!-- WOW ANIMATE STYLE SHEET -->
+	<link rel="stylesheet" href="css/animate.css">
+    <!-- Feather STYLE SHEET -->
+    <link rel="stylesheet" href="css/feather.css">
+    <!-- IMAGE POPUP -->  
+    <link rel="stylesheet" href="css/lc_lightbox.css">
+    <!-- SWIPER SLIDER CSS -->
+    <link rel="stylesheet" href="css/swiper-bundle.min.css">
+    <!-- MAIN STYLE SHEET -->
+    <link rel="stylesheet" href="css/style.css">
     <!-- Particle css -->
     <link rel="stylesheet" href="css/particle-theme.css">
 </head>
@@ -62,21 +73,10 @@
         
         <!-- CONTENT START -->
         <section id="top" class="pa-hero pa-centered-section pa-full-height pa-image-back pushable-content" style="background-image: url(images/hero-dark.jpg);">
-
             <div class="pa-absolute-fill pa-gradient-back-v1" style="opacity: 0;"></div><!-- Increase opacity to overlay hero with a gradient -->
             <div id="particles-js"></div>
 
             <div class="container pa-hero-content text-center">
-                <!-- <div class="row pa-hero-typed-text">
-                    <div class="col-12">
-                        <div class="typed-text mega-typed">
-                            <span class="animated-text-effect">
-                                <span class="typed-cursor">|</span>
-                            </span>
-                        </div>
-                    </div>
-                </div> -->
-                
                 <div class="row">
                     <div class="col-md-8">
                         <div class="text-left">
@@ -90,9 +90,31 @@
                     </div>
                 </div>
             </div>
-
         </section>
         <!-- CONTENT END -->
+
+        <!-- OUR SERVICES SECTION START -->
+        <div class="section-full pt-0 top-2 p-b80 sx-our-services-outer">
+            <div class="container">
+            <!-- TITLE START -->
+            <div class="apos_ment wow fadeIn" data-wow-delay="0s" data-wow-duration="1s">
+                <h1 class="text-center">
+                    <span class="gold-text">About Bluzen<span>
+                </h1>
+                <img class="apos1-home" src="img/apos1.png" title="">
+                <img class="apos2-home" src="img/apos2.png" title="">
+            </div>
+            <!-- <div class="section-head center">
+                <div class="sx-head-s-title">Our Services</div>
+                <div class="sx-head-l-title">
+                <h2 class="sx-title">Sophisticated Knowledge For Rapidly Emerging Legal service of Our IT Solution!</h2>
+                </div>
+            </div> -->
+            <!-- TITLE END -->
+              
+            </div>
+        </div>
+        <!-- OUR SERVICES SECTION END -->
         
        <?php// include('layouts/footer.php') ?>
 
@@ -103,24 +125,39 @@
     </div>
 
 
+
 <!-- JAVASCRIPT  FILES ========================================= --> 
 <!-- JQUERY.MIN JS -->
 <script src="js/jquery-3.6.1.min.js.js"></script>
-<script src="js/popper.min.js"></script><!-- POPPER.MIN JS -->
-<script src="js/bootstrap.min.js"></script><!-- BOOTSTRAP.MIN JS -->
-<script src="js/magnific-popup.min.js"></script><!-- MAGNIFIC-POPUP JS -->
-<script src="js/waypoints.min.js"></script><!-- WAYPOINTS JS -->
-<script src="js/counterup.min.js"></script><!-- COUNTERUP JS -->
-<script src="js/waypoints-sticky.min.js"></script><!-- sticky header JS -->
-<script src="js/isotope.pkgd.min.js"></script><!-- MASONRY  -->
-<script src="js/imagesloaded.pkgd.min.js"></script><!-- MASONRY  -->
-<script src="js/owl.carousel.min.js"></script><!-- OWL  SLIDER  -->
-<script src="js/theia-sticky-sidebar.js"></script><!--sticky content-->
-<script src="js/wow.js"></script><!-- WOW JS -->
-<script src="js/lc_lightbox.lite.js" ></script><!-- IMAGE POPUP -->
-<script src="js/swiper-bundle.min.js"></script><!-- Swiper js -->
-<script src="js/custom.js"></script><!-- CUSTOM FUCTIONS  -->
-<script src="js/particle-theme.js"></script>
+<!-- POPPER.MIN JS -->
+<script src="js/popper.min.js"></script>
+<!-- BOOTSTRAP.MIN JS -->
+<script src="js/bootstrap.min.js"></script>
+<!-- MAGNIFIC-POPUP JS -->
+<script src="js/magnific-popup.min.js"></script>
+<!-- WAYPOINTS JS -->
+<script src="js/waypoints.min.js"></script>
+<!-- COUNTERUP JS -->
+<script src="js/counterup.min.js"></script>
+<!-- sticky header JS -->
+<script src="js/waypoints-sticky.min.js"></script>
+<!-- MASONRY  -->
+<script src="js/isotope.pkgd.min.js"></script>
+<!-- MASONRY  -->
+<script src="js/imagesloaded.pkgd.min.js"></script>
+<!-- OWL  SLIDER  -->
+<script src="js/owl.carousel.min.js"></script>
+<!--sticky content-->
+<script src="js/theia-sticky-sidebar.js"></script>
+<!-- WOW JS -->
+<script src="js/wow.js"></script>
+<!-- IMAGE POPUP -->
+<script src="js/lc_lightbox.lite.js" ></script>
+<!-- Swiper js -->
+<script src="js/swiper-bundle.min.js"></script>
+<!-- CUSTOM FUCTIONS  -->
+<script src="js/custom.js"></script>
+<!-- <script src="js/particle-theme.js"></script> -->
 <script src="js/page-home-dark.js"></script>
 <script src="vendor/particles/particles.min.js"></script>
 
